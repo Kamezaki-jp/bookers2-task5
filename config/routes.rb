96032914977681eab8b4e007025d3b76741ec39a
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-03 05:03:37 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'home#top'
+  get 'home/about'
+  post 'follow/:id'        => 'relationships#follow',    as: 'follow'
+  post 'unfollow/:id'      => 'relationships#unfollow',  as: 'unfollow'
+  get 'user/:id/follows'   => 'relationships#follows',   as: 'follows'
+  get 'user/:id/followers' => 'relationships#followers', as: 'followers'
+  
+  resources :users
+  resources :books do
+    resource    :favorites,     only: [:create, :destroy]
+    resources   :book_comments, only: [:create, :destroy]
+  end
+
+end
